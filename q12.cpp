d4c381b5ee530825881f248d73adb8ff67cbc965
priyankmahour <mahourpriyank40@gmail.com>
2022-12-01 23:43:23 +0530

    all subsequences with sum=k

---
+++ q12.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsequenc_ssum_k(int i, vector<int>&vec,int arr[],int sum,int k,int n)
+{
+    if(i==n)
+    {
+        if(sum==k)
+        {
+            cout<<"{";
+            for(auto it: vec)
+            {
+                cout<<it<<",";
+            }
+            cout<<"}"<<endl;
+        }
+        return;
+    }
+
+    vec.push_back(arr[i]);
+    sum+=arr[i];
+    subsequenc_ssum_k(i+1,vec,arr,sum,k,n);
+
+    //backtrack
+    vec.pop_back();
+    sum-=arr[i];
+    subsequenc_ssum_k(i+1,vec,arr,sum,k,n);
+
+
+
+}
+
+
+int main()
+{
+    int n,k;        
+    cin>>n>>k;      // 3 2
+    int arr[n];     // 1 2 1
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    
+    vector<int> vec;
+    int sum=0;
+
+    subsequenc_ssum_k(0,vec,arr,sum,k,n);
+    return 0;
+}
+
+
